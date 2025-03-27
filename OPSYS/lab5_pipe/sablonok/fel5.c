cadeec9ccd6752adcdba792bca8598e395c58c0d
progenor <nero6126@gmail.com>
2025-03-27 09:44:26 +0200

    done with lab 6

--- OPSYS/lab5_pipe/sablonok/fel5.c
+++ OPSYS/lab5_pipe/sablonok/fel5.c
@@ -1,4 +1,4 @@
-/* 
+/*
    5. feladat sablon
    2 fiú szinkronizálása pipe-al
 
@@ -17,76 +17,82 @@
 
 int main(int argc, char *argv[])
 {
-  int pfd[2]; //pipe azonosítók
+  int pfd[2]; // pipe azonosítók
   pid_t p1, p2;
   char b;
 
-  setbuf(stdout, NULL); //pufferelés tiltása
+  setbuf(stdout, NULL); // pufferelés tiltása
 
-  //cső
-  if (pipe(pfd) < 0) syserr("pipe");
+  // cső
+  if (pipe(pfd) < 0)
+    syserr("pipe");
 
-  //első fiú
-  if ((p1 = fork()) < 0) syserr("p1:");
+  // első fiú
+  if ((p1 = fork()) < 0)
+    syserr("p1:");
 
-  //első fiú folyamat
+  // első fiú folyamat
   if (p1 == 0)
   {
-    //olvasni fog a csőből
-    if (close(pfd[1])<0) syserr("close");
+    // olvasni fog a csőből
+    if (close(pfd[1]) < 0)
+      syserr("close");
 
     printf("fiú 1 vár\n");
-    //próbál egy byte-ot olvasni, blokkolódik
-    if (read(pfd[0], &b, 1) < 0) syserr("read2");
+    // próbál egy byte-ot olvasni, blokkolódik
+    if (read(pfd[0], &b, 1) < 0)
+      syserr("read2");
 
-    //TODO fájl kiolvasása  
+    // TODO fájl kiolvasása
 
     printf("fiú 1 fut\n");
     close(pfd[0]);
     exit(EXIT_SUCCESS);
   }
 
-  //apa folytatja, 2. fiú létrehozása
+  // apa folytatja, 2. fiú létrehozása
   if ((p2 = fork()) < 0)
     syserr("p2:");
 
-  //fiú 2
+  // fiú 2
   if (p2 == 0)
   {
-    //olvasni fog
-    if (close(pfd[1])<0) syserr("close");
-    
+    // olvasni fog
+    if (close(pfd[1]) < 0)
+      syserr("close");
+
     printf("fiú 2 vár\n");
-    //olvasni próbál, blokkolódik
-    if (read(pfd[0], &b, 1) < 0) syserr("read1");
-    
-    //TODO fájl kiolvasása  
-    
+    // olvasni próbál, blokkolódik
+    if (read(pfd[0], &b, 1) < 0)
+      syserr("read1");
+
+    // TODO fájl kiolvasása
+
     printf("fiú 2 fut\n");
     close(pfd[0]);
     exit(0);
   }
 
-  //apa dolgozik
+  // apa dolgozik
 
-  //írni fog a csőbe
-  if (close(pfd[0])<0) syserr("close");
+  // írni fog a csőbe
+  if (close(pfd[0]) < 0)
+    syserr("close");
 
-  //apa
+  // apa
   printf("apa dolgozik\n");
 
-  //TODO fájl létrehozása
-
+  // TODO fájl létrehozása
 
   sleep(5);
   printf("apa befejezte\n");
 
-  //ha lezárja a csövet, a két read visszatér fájl vége 0-val
-  //és a két fiú folyamat újra indul
-  //ha írtunk volna 2 byte-ot és utána zárjuk a csővet ugynaz történik
+  // ha lezárja a csövet, a két read visszatér fájl vége 0-val
+  // és a két fiú folyamat újra indul
+  // ha írtunk volna 2 byte-ot és utána zárjuk a csővet ugynaz történik
   close(pfd[1]);
 
-  //megvárja a fiait
+  // megvárja a fiait
   wait(NULL);
   wait(NULL);
   exit(EXIT_SUCCESS);
